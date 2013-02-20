a3582b45f07c87abbffbac6bfec762d8e5ad325e
jackiekazil <jackiekazil@gmail.com>
2013-02-19 19:17:50 -0500

    Updating agents to remove extra fields & to store games played.

--- ziagent_game/agents.py
+++ ziagent_game/agents.py
@@ -1,28 +1,30 @@
 import random as r
 from collections import defaultdict
-import weakref
 
 
 from ziagent_game.common import *
 
 
 class Agent(object):
-    "Object class for agents."
+    """
+    Object class for agents.
+    Agents are made of:
+        id
+        total payoff -- winnings from all games
+        games played -- list of game id, move, payoff
+    """
     instances = {}
 
     def __init__(self):
-        
-        self.move = r.randint(0,1)
-        self.previous_moves = []
+        self.total_payoff = 0
+        self.games_played = []
 
         self.get_id()
         self.__class__.instances[self.id] = self
 
     def generate_move(self):
-        previous_move = self.move
-        self.previous_moves.append(previous_move)
-        
         self.move = r.choice(move_choices)
+        return self.move
 
     def get_id(self):
          self.id = get_id(self.__class__)
