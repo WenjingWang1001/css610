30e5e6ce2a583580010c1c546eea72b491564155
jackiekazil <jackiekazil@gmail.com>
2013-02-19 19:17:16 -0500

    Updating moves & payoffs.

--- ziagent_game/common.py
+++ ziagent_game/common.py
@@ -2,14 +2,21 @@
     This file is used to store default settings.
 '''
 
-__all__ = ['nagents', 'move_choices', 'get_id']
+__all__ = ['nagents', 'move_choices', 'get_id', 'payoffs']
 
 nagents = 100
 
 # Game select
 move_choices = (
-    'swerve',
-    'straight',
+    ('swerve', 0),
+    ('straight', 1),
+)
+
+payoffs = (
+    ((0,0), (1,1)),     #Both players swerve
+    ((0,1), (0,2)),     #a1 swerves, a2 goes straight
+    ((1,0), (2,0)),     #a1 goes straight, a2 swerves
+    ((1,1), (-1,-1)),   #Both players go straight
 )
 
 
